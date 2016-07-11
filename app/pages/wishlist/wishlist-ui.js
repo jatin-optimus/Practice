94d36b1a39548df05187f8e36adaff3ef0113e5f
jatin-optimus <jatin.chitkara@optimusinfo.com>
2016-07-11 15:27:36 +0530

    removal of unnecessary files

--- app/pages/wishlist/wishlist-ui.js
+++ app/pages/wishlist/wishlist-ui.js
@@ -1,11 +1,11 @@
-define([
-    '$',
-    'global/ui/wishlist-create-list-modal-ui'
-], function($, wishlistCreateListModalUI) {
-
-    var wishlistUI = function() {
-        wishlistCreateListModalUI();
-    };
-
-    return wishlistUI;
-});
+// define([
+//     '$',
+//     'global/ui/wishlist-create-list-modal-ui'
+// ], function($, wishlistCreateListModalUI) {
+//
+//     var wishlistUI = function() {
+//         wishlistCreateListModalUI();
+//     };
+//
+//     return wishlistUI;
+// });
