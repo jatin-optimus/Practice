84b543647aff13813efe98bbc0fb293dba6be728
Aastha Sharma <aastha.sharma@optimusinfo.com>
2016-07-22 12:17:59 +0530

    styling

--- app/pages/product-details/product-details-ui.js
+++ app/pages/product-details/product-details-ui.js
@@ -63,16 +63,34 @@ define([
             var $closeButton = $addToCartPinny.find('.pinny__close');
             $closeButton.click();
         });
+
+        // Opening the video bellow by default to match mock.
+        // $('.js-product-description-bellows').bellows('open', 0);
+
+
     };
+
+    var initBellows = function() {
+        $('.c-bellows__item').map(function(_, item) {
+            var $item = $(item);
+            var $icon = $item.find('.c-icon');
+            if ($item.hasClass('bellows--is-open')) {
+                $icon.attr('data-fallback', 'img/png/collapse.png');
+                $icon.find('title').text('collapse');
+                $icon.find('use').attr('xlink:href', '#icon-collapse');
+            }
+        });
+    };
+    // SetTimeout is used because the video bello has been opened
+    // just as the pageload and was takibg some time to chage the icon and holding it.
+
+    setTimeout(initBellows, 500);
 
     var interceptAddToCart = function interceptAddToCart() {
 
         var _override  = window.updateShoppingCartSummary;
         window.updateShoppingCartSummary = function() {
             var override = _override.apply(this, arguments);
-            if ($('#addToCartButton').attr('src').indexOf('_gr.gif') >= 0) {
-                $('.prod_add_to_cart').addClass('c--disabled');
-            }
             var $modal = $('#addToCartInfo');
             var $content = $('#addToCartInfoCont');
             $content.find('#continueShoppingLink').insertAfter('#viewCartLink');
@@ -80,34 +98,36 @@ define([
             $addToCartPinny.find('.c-sheet__title').html('Added to Cart');
             $addToCartPinny.find('.js-added-to-cart-pinny__body').html($content);
             $addToCartPinny.find('.pinny__close').addClass('container-close');
-            if (!$('.js-added-to-cart-pinny').hasClass('js-rendered')) {
+            if (!$addToCartPinny.hasClass('js-rendered')) {
                 $addToCartPinny.pinny('open');
             }
-            $('.js-added-to-cart-pinny').addClass('js-rendered');
+            $addToCartPinny.addClass('js-rendered');
             return _override;
         };
 
     };
 
     var interceptCheckAddToCart = function interceptCheckAddToCart() {
+        var $addToCartButtonContainer = $('.prod_add_to_cart');
 
         var _override  = window.checkAddToCart;
         window.checkAddToCart = function() {
             var override = _override.apply(this, arguments);
             if ($('#addToCartButton').attr('src').indexOf('_gr.gif') >= 0) {
-                $('.prod_add_to_cart').addClass('c--disabled');
+                $addToCartButtonContainer.addClass('c--disabled');
             } else {
-                $('.prod_add_to_cart').removeClass('c--disabled');
+                $addToCartButtonContainer.removeClass('c--disabled');
             }
             return _override;
         };
     };
 
     var createSwatchesSection = function() {
-        $('.s7flyoutSwatches').addClass('c-scroller');
-        $('.s7flyoutSwatches').find('div').first().addClass('c-scroller__content').removeAttr('style');
-        $('.s7flyoutSwatches').find('div').first().find('> div').last().addClass('c-slideshow').removeAttr('style');
-        $('.s7flyoutSwatch').each(function() {
+        var $swatchesContainer = $('.s7flyoutSwatches');
+        $swatchesContainer.addClass('c-scroller');
+        $swatchesContainer.find('div').first().addClass('c-scroller__content').removeAttr('style');
+        $swatchesContainer.find('div').first().find('> div').last().addClass('c-slideshow').removeAttr('style');
+        $swatchesContainer.find('.s7flyoutSwatch').each(function() {
             $(this).addClass('c-slideshow__slide');
             $(this).parent().addClass('c-swatches').removeAttr('style');
             $('.c-swatches').parent().removeAttr('style');
@@ -127,7 +147,7 @@ define([
     var buildYouMayAlsoLikeCarousel = function() {
         var $container = $('.js-suggested-products');
         var $parsedProducts = [];
-        var $heading = $('<h2 class="c-carousel__title">').text('You Might Also Like');
+        var $heading = $('<h2 class="c-carousel__title">').text('You May Also Like');
         var productTileData = [];
         setTimeout(function() {
             if ($('#PRODPG1_cm_zone').children().length === 0) {
@@ -168,26 +188,10 @@ define([
             },
             closed: function() {
                 $('#addToCartInfo_mask').css('display', 'none');
-                $('.js-added-to-cart-pinny').removeClass('js-rendered');
+                $addToCartPinny.removeClass('js-rendered');
             }
         });
     };
-
-
-    // Shows video bello open and change '+' svg to '-' svg
-    var initBellows = function() {
-        $('.c-bellows__item').map(function(_, item) {
-            var $item = $(item);
-            var $icon = $item.find('.c-icon');
-            $('.js-video-bellows').addClass('bellows--is-open');
-            if ($item.hasClass('bellows--is-open')) {
-                $icon.attr('data-fallback', 'img/png/collapse.png');
-                $icon.find('title').text('collapse');
-                $icon.find('use').attr('xlink:href', '#icon-collapse');
-            }
-        });
-    };
-
 
     var productDetailsUI = function() {
         buildYouMayAlsoLikeCarousel();
@@ -197,7 +201,6 @@ define([
         interceptAddToCart();
         interceptCheckAddToCart();
         interceptSwatchCreation();
-        initBellows();
     };
 
     return productDetailsUI;
